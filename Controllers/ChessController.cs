41d511adf6b7e1d9b4be25cd385446e1de57d46f
Tosha-84 <vilachev2015@gmail.com>
2023-10-05 00:52:04 +0800

    StartWorkWithChess

---
+++ Controllers/ChessController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FashionChess.Controllers
+{
+    public class ChessController : Controller
+    {
+        public class ChessBoard
+        {
+            private class Cell
+            {
+                bool 
+            }
+        }
+
+        private interface IChessFigure
+        {
+            public void choose();
+            public void Move();
+        }
+
+        private abstract class ChessFigure : IChessFigure
+        {
+            private byte _xPosition { get; set; }
+            private byte _yPosition { get; set; }
+
+
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
